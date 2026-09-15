6f9ddf2d7645121942a1f4dcd73ae7de8af64018
Tim Smith <tim@mondoo.com>
2026-09-15 14:05:27 -0700

    Ignore gem options when resolving cookbook gem metadata (#52)
    A cookbook's gem metadata accepts the same trailing options Hash a
    Gemfile does, for example:
    
      gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
    
    Chef passes those options through to a generated Gemfile, but ChefSpec
    handed them to Gem::Specification.find_by_name as if they were version
    requirements. Gem::Requirement then raised BadRequirementError, which is
    not rescued, so the whole converge failed for any cookbook, or dependent
    cookbook, using gem options.
    
    Drop the options Hash before resolving the gem.
    
    Fixes chefspec/chefspec#1016
    
    Signed-off-by: Tim Smith <tsmith84@proton.me>

---
+++ examples/cookbook_gems/metadata.rb
@@ -0,0 +1,6 @@
+name 'cookbook_gems'
+version '1.0.0'
+
+# Gem declarations accept the same trailing options Hash a Gemfile does.
+gem 'rspec'
+gem 'chef-utils', platforms: %i{mingw mswin x64_mingw}
---
+++ examples/cookbook_gems/recipes/default.rb
@@ -0,0 +1,1 @@
+log 'cookbook_gems'
